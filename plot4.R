a4d24a3ce27fe2afbb5ec0f8953da6f32b842d10
data478 <igor.milos.git@gmail.com>
2014-05-10 20:06:30 +0200

    explanatory comments about differences from reference plots

--- plot4.R
+++ plot4.R
@@ -3,6 +3,10 @@ source("script/load_data.R")
 png(filename="plot4.png", width=520, height=520)
 attach(hpcons)
 
+# not duplicating the code: use the same commands for producing
+# the individual plots
+# (this is the reason my plots differ somewhat from the reference plots)
+
 par(mfrow=c(2,2), cex=.9)
 source("script/plot_global_active_power.R")
 source("script/plot_voltage.R")
