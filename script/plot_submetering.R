aba2c65ece0f78e50f6582ad90c1d92bfba3fd93
data478 <igor.milos.git@gmail.com>
2014-05-10 19:42:58 +0200

    comment regarding bty="n" (no legend box)

--- script/plot_submetering.R
+++ script/plot_submetering.R
@@ -6,5 +6,5 @@ points(Sub_metering_3 ~ DateTime, ylab="Energy sub metering", ty="l", col="blue"
 legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
        col=c("black", "red", "blue"),
        lty=1, # use this to have the line appear in the legend
-       bty="n"
+       bty="n" # no box around the legend: looks neater, esp in plot4
        )
